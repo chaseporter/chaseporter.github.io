3ba69cafcf52833be9e3d252907b54b64f69cf1d
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2019-12-06 12:26:04 -0800

    - Fixes title in tab on browser - Updates style of recently read and adds authors - Edits order and cropping of photos in gallery

--- src/components.js
+++ src/components.js
@@ -14,37 +14,36 @@ const RecentReads = () => {
             <p style={{margin: "20px"}}>
                 Currently Reading
                 <br/>
-                - Alexander Pushkin 
-                - Their Eyes Were Watching God
+                - <i>Novels, Tales, and Journeys</i> by Alexander Pushkin 
+                - <i>Their Eyes Were Watching God</i> by Zora Neale Hurston 
+                - <i>A People's History of the United States</i> by Howard Zinn 
+                - <i>I Know Why the Caged Bird Sings</i> by Maya Angelou - 
                 <br/>
                 <br/>
                 Recently Read
                 <br/>
-                    - Pachinko
-                    - The Color of Law
-                    - Another Country
-                    - We Were 8 Years in Power
-                    - A History of Japan
-                    - Globalization and Its Discontents 
-                    - A People's History of the United States
-                    - Hopes and Prospects
-                    - Americanah
-                    - Maya Angelou's poetry
-                    - I Know Why the Caged Bird Sings 
-                    - Langston Hughs poetry
-                    - Notes of a Native Son
-                    - The Things We Don't Do
-                    - The Road
-                    - Women of Crisis
-                    - Sophie's World
-                    - The Prophet
+                    - <i>Pachinko</i> by Min Jin Lee
+                    - <i>The Color of Law</i> by Richard Rothstein
+                    - <i>Another Country</i> and <i>Notes of a Native Son</i> by James Baldwin
+                    - <i>We Were 8 Years in Power</i> by Ta-Nehisi Coates
+                    - <i>Showa: A History of Japan</i> by Shigeru Mizuki
+                    - <i>Globalization and Its Discontents</i> by Joseph Stiglitz
+                    - <i>Hopes and Prospects</i> by Noam Chomsky
+                    - <i>Americanah</i> by Chimamanda Ngozi Adichie
+                    - <i>Selected Poems</i> by Langston Hughs 
+                    - <i>The Things We Don't Do</i> by Andres Neuman
+                    - <i>The Road</i> by Cormac McCarthy
+                    - <i>Women of Crisis: Lives of Struggle and Hope</i> by Robert Coles
+                    - <i>Sophie's World</i> by Jostein Gardner
+                    - <i>Siddhartha</i> by Hermann Hesse -
                 <br/>
                 <br/>
                 To Read
                 <br/>
-                    - House of the Spirits
-                    - The Uninhabitable Earth
-                    - The Fire Next Time
+                    - <i>House of the Spirits</i> by Isabel Allende
+                    - <i>The Uninhabitable Earth: Life After Warming</i> by David Wallace-Wells 
+                    - <i>The Fire Next Time</i> by James Baldwin
+                    - <i>The Samurai's Garden</i> by Gale Tsukiyama -
             </p>
         </div>
     )
